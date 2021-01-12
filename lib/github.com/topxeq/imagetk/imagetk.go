c3946864b2079c81c5eaf101b607465c8808e257
unknown <topxeq@163.com>
2021-01-12 13:43:09 +0800

    2021011201

--- lib/github.com/topxeq/imagetk/imagetk.go
+++ lib/github.com/topxeq/imagetk/imagetk.go
@@ -11,12 +11,23 @@ import (
 var Exports = map[string]interface{}{
 	"_name": "github.com/topxeq/imagetk",
 
+	"BOTTOM":            imagetk.BOTTOM,
+	"BOTTOM_LEFT":       imagetk.BOTTOM_LEFT,
+	"BOTTOM_RIGHT":      imagetk.BOTTOM_RIGHT,
 	"Bicubic":           imagetk.Bicubic,
 	"Bilinear":          imagetk.Bilinear,
+	"CENTER":            imagetk.CENTER,
+	"LEFT":              imagetk.LEFT,
 	"Lanczos2":          imagetk.Lanczos2,
 	"Lanczos3":          imagetk.Lanczos3,
 	"MitchellNetravali": imagetk.MitchellNetravali,
 	"NearestNeighbor":   imagetk.NearestNeighbor,
+	"RIGHT":             imagetk.RIGHT,
+	"TOP":               imagetk.TOP,
+	"TOP_LEFT":          imagetk.TOP_LEFT,
+	"TOP_RIGHT":         imagetk.TOP_RIGHT,
+
+	"ITKX": imagetk.ITKX,
 
 	"ImageTK":    qlang.StructOf((*imagetk.ImageTK)(nil)),
 	"imagetk":    imagetk.NewImageTK,
--- lib/github.com/topxeq/sqltk/sqltk.go
+++ lib/github.com/topxeq/sqltk/sqltk.go
@@ -2,6 +2,8 @@ package sqltk
 
 import (
 	"github.com/topxeq/sqltk"
+
+	qlang "github.com/topxeq/qlang/spec"
 )
 
 // Exports is the export table of this module.
@@ -12,12 +14,20 @@ var Exports = map[string]interface{}{
 	"ConnectDB":          sqltk.ConnectDB,
 	"ConnectDBNoPing":    sqltk.ConnectDBNoPing,
 	"ExecV":              sqltk.ExecV,
+	"FormatSQLValue":     sqltk.FormatSQLValue,
+	"GetVersion":         sqltk.GetVersion,
+	"NewSqlTK":           sqltk.NewSqlTK,
 	"OneLineRecordToMap": sqltk.OneLineRecordToMap,
 	"QueryDBCount":       sqltk.QueryDBCount,
 	"QueryDBI":           sqltk.QueryDBI,
 	"QueryDBNS":          sqltk.QueryDBNS,
 	"QueryDBNSS":         sqltk.QueryDBNSS,
+	"QueryDBNSSF":        sqltk.QueryDBNSSF,
 	"QueryDBNSV":         sqltk.QueryDBNSV,
 	"QueryDBS":           sqltk.QueryDBS,
 	"QueryDBString":      sqltk.QueryDBString,
+	"RecordsToMapArray":  sqltk.RecordsToMapArray,
+	"SqlTKX":             sqltk.SqlTKX,
+
+	"SqlTK": qlang.StructOf((*sqltk.SqlTK)(nil)),
 }
