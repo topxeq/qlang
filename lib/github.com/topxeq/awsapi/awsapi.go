4cff88c1280ad5bea786d71fcdf86e95cf1560d3
unknown <topxeq@163.com>
2021-02-06 09:26:44 +0800

    2021020601

---
+++ lib/github.com/topxeq/awsapi/awsapi.go
@@ -0,0 +1,13 @@
+package awsapi
+
+import (
+	"github.com/topxeq/awsapi"
+)
+
+// Exports is the export table of this module.
+//
+var Exports = map[string]interface{}{
+	"_name": "github.com/topxeq/awsapi",
+
+	"Sign": awsapi.Sign,
+}
