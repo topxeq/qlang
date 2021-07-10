11efe291d569c0b1433977c2578d98259ce025e2
unknown <topget@sina.com>
2021-07-10 17:41:17 +0800

    2021071003

--- lib/github.com/topxeq/tk/tk.go
+++ lib/github.com/topxeq/tk/tk.go
@@ -30,6 +30,7 @@ var Exports = map[string]interface{}{
 	"CalCosineSimilarityBetweenFloatsBig": tk.CalCosineSimilarityBetweenFloatsBig,
 	"CheckErr":                            tk.CheckErr,
 	"CheckErrCompact":                     tk.CheckErrCompact,
+	"CheckErrStr":                         tk.CheckErrStr,
 	"CheckErrStrf":                        tk.CheckErrStrf,
 	"CheckErrf":                           tk.CheckErrf,
 	"CheckError":                          tk.CheckError,
