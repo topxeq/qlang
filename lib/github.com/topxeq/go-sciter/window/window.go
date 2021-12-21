e3b80981c2891dfd6dce9f525c6bb5376cbffdb5
unknown <topxeq@163.com>
2021-12-21 16:08:55 +0800

    2021122103

---
+++ lib/github.com/topxeq/go-sciter/window/window.go
@@ -0,0 +1,13 @@
+package window
+
+import (
+	"github.com/topxeq/go-sciter/window"
+)
+
+// Exports is the export table of this module.
+//
+var Exports = map[string]interface{}{
+	"_name": "github.com/topxeq/go-sciter/window",
+
+	"New": window.New,
+}
