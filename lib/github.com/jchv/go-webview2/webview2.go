98b7a7d0bf41ee295d7b8875df17e1ac34a2088a
unknown <topxeq@163.com>
2023-01-06 10:00:12 +0800

    2023010601

---
+++ lib/github.com/jchv/go-webview2/webview2.go
@@ -0,0 +1,23 @@
+package webview2
+
+import (
+	"github.com/jchv/go-webview2"
+	qlang "github.com/topxeq/qlang/spec"
+)
+
+// Exports is the export table of this module.
+var Exports = map[string]interface{}{
+	"_name": "github.com/jchv/go-webview2",
+
+	"HintFixed": webview2.HintFixed,
+	"HintMax":   webview2.HintMax,
+	"HintMin":   webview2.HintMin,
+	"HintNone":  webview2.HintNone,
+
+	"New":            webview2.New,
+	"NewWindow":      webview2.NewWindow,
+	"NewWithOptions": webview2.NewWithOptions,
+
+	"WindowOptions":  qlang.StructOf((*webview2.WindowOptions)(nil)),
+	"WebViewOptions": qlang.StructOf((*webview2.WebViewOptions)(nil)),
+}
