8b4ca2d80d19d58c021f8f17f2bec73d855acd5e
unknown <topxeq@163.com>
2023-01-06 10:21:12 +0800

    2023010601

---
+++ lib/github.com/jchv/go-webview2/webview2_nonwindows.go
@@ -0,0 +1,7 @@
+//go:build !windows
+// +build !windows
+
+package webview2
+
+// Exports is the export table of this module.
+var Exports = map[string]interface{}{}
--- lib/github.com/jchv/go-webview2/webview2_windows.go
+++ lib/github.com/jchv/go-webview2/webview2_windows.go
@@ -1,3 +1,6 @@
+//go:build windows
+// +build windows
+
 package webview2
 
 import (
