cc7e635a418e95ccb98b51555ffa949ec9ad4e71
unknown <topxeq@163.com>
2023-03-06 16:43:35 +0800

    2023030601

--- lib/gonum.org/v1/plot/vg/vg.go
+++ lib/gonum.org/v1/plot/vg/vg.go
@@ -7,7 +7,6 @@ import (
 )
 
 // Exports is the export table of this module.
-//
 var Exports = map[string]interface{}{
 	"_name": "gonum.org/v1/plot/vg",
 
@@ -20,15 +19,15 @@ var Exports = map[string]interface{}{
 	"Millimeter": vg.Millimeter,
 	"MoveComp":   vg.MoveComp,
 
-	"FontDirs": vg.FontDirs,
-	"FontMap":  vg.FontMap,
+	// "FontDirs": vg.FontDirs,
+	// "FontMap":  vg.FontMap,
 
-	"AddFont":    vg.AddFont,
+	// "AddFont":    vg.AddFont,
 	"Initialize": vg.Initialize,
 
-	"MakeFont":    vg.MakeFont,
-	"FontExtents": qlang.StructOf((*vg.FontExtents)(nil)),
-	"PathComp":    qlang.StructOf((*vg.PathComp)(nil)),
-	"Point":       qlang.StructOf((*vg.Point)(nil)),
-	"Rectangle":   qlang.StructOf((*vg.Rectangle)(nil)),
+	// "MakeFont":    vg.MakeFont,
+	// "FontExtents": qlang.StructOf((*vg.FontExtents)(nil)),
+	"PathComp":  qlang.StructOf((*vg.PathComp)(nil)),
+	"Point":     qlang.StructOf((*vg.Point)(nil)),
+	"Rectangle": qlang.StructOf((*vg.Rectangle)(nil)),
 }
